891287ac2901bdd80ff03a1caf0f2b83fff46cc9
Linos Giannopoulos <lgian@noc.grnet.gr>
2018-04-17 01:46:13 +0300

    Fixed out of bounds neighbouring nodes

--- map.go
+++ map.go
@@ -9,7 +9,7 @@ const (
 	Goal     = 3
 )
 
-type Map [][]uint8
+type Map [][]*Node
 
 type Node struct {
 	X, Y    int
@@ -18,13 +18,44 @@ type Node struct {
 	parent  *Node
 }
 
-func (m Map) GetNeighbours(x, y int) []uint8 {
-	fmt.Println(m[x][y-1])
-	nbs := []uint8{
-		m[x+1][y],
-		m[x-1][y],
-		m[x][y+1],
-		m[x][y-1],
+func NewMap(xLen, yLen int) Map {
+	m := make([][]*Node, xLen)
+	for i := range m {
+		m[i] = make([]*Node, yLen)
+	}
+	return m
+}
+
+func (m Map) GetNeighbours(x, y int) []Node {
+	nbs := make([]Node, 0)
+	if x-1 >= 0 && m[x-1][y].Type != Obstacle {
+		nbs = append(nbs, *m[x-1][y])
+	}
+	if x+1 < len(m) && m[x+1][y].Type != Obstacle {
+		nbs = append(nbs, *m[x+1][y])
+	}
+	if y-1 >= 0 && m[x][y-1].Type != Obstacle {
+		nbs = append(nbs, *m[x][y-1])
+	}
+	if y+1 < len(m[0]) && m[x][y+1].Type != Obstacle {
+		nbs = append(nbs, *m[x][y+1])
 	}
 	return nbs
+}
+func (m Map) drawMap() {
+	for y := range m {
+		for x := range m[y] {
+			switch m[x][y].Type {
+			case Tile:
+				fmt.Print(".")
+			case Start:
+				fmt.Print("0")
+			case Goal:
+				fmt.Print("X")
+			default:
+				fmt.Print("#")
+			}
+		}
+		fmt.Println()
+	}
 }
